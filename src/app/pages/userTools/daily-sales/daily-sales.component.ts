48bf9a2f63e9e30b66ac63b3ddacc6f6cc5e4ae2
Angel Gonzalez <angelgonzalezh08@gmail.com>
2024-07-01 21:50:42 -0700

    Estilos venta y descarga de PDF

--- src/app/pages/userTools/daily-sales/daily-sales.component.ts
+++ src/app/pages/userTools/daily-sales/daily-sales.component.ts
@@ -1,6 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CashRegisterService } from 'src/app/services/cash-register.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+
+interface Sale {
+  date: Date;
+  total: number;
+  paymentMethod: string;
+}
 
 @Component({
   selector: 'app-daily-sales',
@@ -32,5 +40,36 @@ export class DailySalesComponent {
         console.error('Error fetching open cash register with sales', error);
       }
     );
+  }
+
+  generarPDF() {
+    const doc = new jsPDF();
+    doc.setFontSize(18);
+    doc.text('Historial de Ventas Diarias', 14, 22);
+    doc.setFontSize(12);
+    doc.text(`Usuario: ${this.usuario}`, 14, 30);
+    doc.text(`Fecha de Inicio: ${new Date(this.openCashRegisterWithSales.startDate).toLocaleDateString()}`, 14, 36);
+    doc.text(`Monto Inicial $: ${this.openCashRegisterWithSales.initialAmount}`, 14, 42);
+
+    // Subtotal
+    const subtotal = this.openCashRegisterWithSales.sales.reduce((sum: number, sale: Sale) => sum + sale.total, 0);
+
+    (doc as any).autoTable({
+      head: [['Fecha', 'Monto', 'Metodo de Pago']],
+
+      body: [
+        ...this.openCashRegisterWithSales.sales.map((sale: Sale) => [
+          new Date(sale.date).toLocaleString(),
+          sale.total.toFixed(2),
+          sale.paymentMethod
+        ]),
+
+        [{ content: 'Subtotal', colSpan: 1, style: { haling: 'right', fontSize: 'bold' } }, { content: subtotal.toFixed(2), colSpan: 1, style: { haling: 'right', fontSize: 'bold' } }, '']
+      ],
+
+      startY: 50,
+      theme: 'plain',
+    });
+    doc.save('Historial_de_Ventas_Diarias.pdf');
   }
 }
