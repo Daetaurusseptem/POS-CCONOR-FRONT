64b5d67b18ac8dd91ad347e31700b8f2b723addf
Angel Gonzalez <angelgonzalezh08@gmail.com>
2024-07-19 21:37:04 -0600

    tracking de ingredientes

--- src/app/pages/adminTools/statistics/statistics.component.ts
+++ src/app/pages/adminTools/statistics/statistics.component.ts
@@ -4,6 +4,7 @@ import { Chart, ChartData, ChartOptions, ChartType, LabelItem, registerables } f
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { ChartDataset } from 'chart.js';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-statistics',
@@ -12,10 +13,12 @@ import { ChartDataset } from 'chart.js';
 })
 export class StatisticsComponent {
   topSellingProducts: any[] = [];
+  ingredientsStatistics: any[] = [];
   year: number = new Date().getFullYear();
   week: number = this.getWeekNumber(new Date());
   searchForm!: FormGroup;
   companyId: string;
+  isProductView: boolean = true;
 
   // Chart.js variables
   barChartOptions: ChartOptions = {
@@ -37,6 +40,7 @@ export class StatisticsComponent {
     private authService: AuthService
   ) {
     this.companyId = this.authService.companyId!;
+    Chart.register(...registerables);
   }
 
   ngOnInit(): void {
@@ -55,10 +59,33 @@ export class StatisticsComponent {
     });
   }
 
+  loadIngredientsStatistics(): void {
+    this.statisticsService.getIngredientsStatistics().subscribe(data => {
+      this.ingredientsStatistics = data.ingredients;
+      this.updateChartData();
+    });
+  }
+
   updateChartData(): void {
-    this.barChartLabels = this.topSellingProducts.map(product => product.product.name);
-    this.barChartData.labels = this.barChartLabels;
-    this.barChartData.datasets[0].data = this.topSellingProducts.map(product => product.totalQuantity);
+    if (this.isProductView) {
+      this.barChartLabels = this.topSellingProducts.map(product => product.product.name);
+      this.barChartData.labels = this.barChartLabels;
+      this.barChartData.datasets[0].data = this.topSellingProducts.map(product => product.totalQuantity);
+    } else {
+      this.barChartLabels = this.ingredientsStatistics.map(ingredient => ingredient._id);
+      this.barChartData.labels = this.barChartLabels;
+      this.barChartData.datasets[0].data = this.ingredientsStatistics.map(ingredient => ingredient.totalStock);
+      this.barChartData.datasets[0].label = 'Stock Total';
+    }
+  }
+
+  toggleView(): void {
+    this.isProductView = !this.isProductView;
+    if (this.isProductView) {
+      this.loadTopSellingProducts();
+    } else {
+      this.loadIngredientsStatistics();
+    }
   }
 
   getWeekNumber(date: Date): number {
@@ -92,7 +119,33 @@ export class StatisticsComponent {
     this.searchForm.patchValue({ year: this.year, week: this.week });
     this.loadTopSellingProducts();
   }
-  downloadCsvWhitPage(){
-    this.statisticsService.downloadCsv(this.topSellingProducts,"recuentos");
+
+  downloadExcel() {
+    const data = this.isProductView ? this.topSellingProducts : this.ingredientsStatistics;
+    const sheetName = this.isProductView ? 'Top Productos' : 'Estadísticas de Ingredientes';
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
+      [this.isProductView ? `Productos mas vendidos - Semana ${this.week}, ${this.year}` : 'Estadísticas de Ingredientes'],
+      [],
+      this.isProductView ? ['Ranking', 'Producto', 'Cantidad Vendida'] : ['Ranking', 'Ingrediente', 'Stock Total', 'Valor Total'],
+      ...data.map((item, index) => this.isProductView ?
+        [index + 1, item.product.name, item.totalQuantity] :
+        [index + 1, item._id, item.totalStock, item.totalValue]
+      )
+    ]);
+
+    ws['!cols'] = this.isProductView ? [{ wch: 10 }, { wch: 40 }, { wch: 20 }] : [{ wch: 10 }, { wch: 40 }, { wch: 20 }, { wch: 20 }];
+    ws['!merges'] = [{ s: { r: 0, c: 0 }, e: { r: 0, c: this.isProductView ? 2 : 3 } }];
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, sheetName);
+
+    wb.Props = {
+      Title: this.isProductView ? `Productos mas vendidos - Semana ${this.week}, ${this.year}` : 'Estadísticas de Ingredientes',
+      Subject: "Reporte de Ventas",
+      Author: "Leonix64 & Edy",
+      CreatedDate: new Date()
+    };
+
+    XLSX.writeFile(wb, `${sheetName}_${this.week}_${this.year}.xlsx`);
   }
 }
